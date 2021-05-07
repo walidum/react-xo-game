77c541dbc1def3763300b4216cea67374b2cd219
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-07 10:22:47 +0100

    add game component

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -3,7 +3,10 @@ import './style.css'
 const Game = () => {
     return (
         <div className={'g-container'}>
-
+            <div className={'g-head'}>
+                <span>X-O GAME</span>
+                <span>X Turn</span>
+            </div>
         </div>
 
     )
