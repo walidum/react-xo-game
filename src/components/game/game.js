58b58a40decd88628607fee5ffcc5730147c6838
Walid Boubechtoula <dw_boubechtoula@esi.dz>
2021-05-07 11:23:51 +0100

    add game component

--- src/components/game/game.js
+++ src/components/game/game.js
@@ -1,8 +1,11 @@
 import './style.css'
 import {Card} from "@material-ui/core";
 import Case from "../case/case";
+import {useState} from "react";
 
 const Game = () => {
+    const inittab = ['', '', '', '', '', '', '', '', '']
+    const [values, setValues] = useState(inittab)
     return (
         <div className={'g-container'}>
             <div className={'g-head'}>
@@ -12,15 +15,16 @@ const Game = () => {
                     X Turn</span>
             </div>
             <Card className={'g-card'}>
-                <Case/>
-                <Case/>
-                <Case/>
-                <Case/>
-                <Case/>
-                <Case/>
-                <Case/>
-                <Case/>
-                <Case/>
+                <Case key={0} values={values} setValues={setValues}/>
+                <Case key={1} values={values} setValues={setValues}/>
+                <Case key={2} values={values} setValues={setValues}/>
+                <Case key={3} values={values} setValues={setValues}/>
+                <Case key={4} values={values} setValues={setValues}/>
+                <Case key={5} values={values} setValues={setValues}/>
+                <Case key={6} values={values} setValues={setValues}/>
+                <Case key={7} values={values} setValues={setValues}/>
+                <Case key={8} values={values} setValues={setValues}/>
+
             </Card>
         </div>
 
